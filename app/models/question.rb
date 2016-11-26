20b2e964f779186380058d810b32bc8800ec978b
Megan Eding <eding.megan@gmail.com>
2016-11-25 17:33:59 -0800

    create polymorphic assoc for votes/comments, create models, create vote/comment seeds

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ActiveRecord::Base
   validates :user_id, :text, { presence: true }
   belongs_to :user
   has_many :answers
+  has_many :votes, as: :votable
+  has_many :comments, as: :commentable
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 require 'faker'
 
+
 100.times do
   User.create!( :name => Faker::Name.name,
                 :email      => Faker::Internet.email,
                 :password   => 'password' )
 end
+
 
 100.times do
   Answer.create!( :text => Faker::ChuckNorris.fact,
@@ -13,7 +15,26 @@ end
                   :question_id => rand(1..100))
 end
 
+
 100.times do
   Question.create!( :text => Faker::Hipster.sentence,
                     :user_id => rand(1..100))
+end
+
+
+comment_options = [Answer, Question]
+100.times do
+  Comment.create!(  :text => Faker::Hipster.sentence,
+                    :user_id => rand(1..100),
+                    :commentable_id => rand(1..100),
+                    :commentable_type => comment_options.sample)
+end
+
+
+vote_options = [Answer, Question, Comment]
+100.times do
+  Vote.create!(     :user_id => rand(1..100),
+                    :upvote => 1,
+                    :votable_id => rand(1..100),
+                    :votable_type => vote_options.sample)
 end
