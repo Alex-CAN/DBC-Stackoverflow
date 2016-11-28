e3783e8ee053b22e0350f05cef467764f646e43a
Alex Nelson <caminelson.cn@gmail.com>
2016-11-27 22:34:37 -0800

    adding askAnswer box to homepage

--- public/js/application.js
+++ public/js/application.js
@@ -7,42 +7,26 @@ $(document).ready(function() {
   $(tabID).show();
   });
 
-  $('#registration, form').on('submit', function(){
-  event.preventDefault();
-      var $reg_form = $(this);
-      var method = $reg_form.attr('method');
-      var action = $reg_form.attr('action');
-      var data = $reg_form.attr('submit');
-      $('.tab-content').hide()
-  console.log(method)
-  console.log(action)
-  console.log(data)
+  // $('#registration, form').on('submit', function(){
+  // event.preventDefault();
+  //     var $reg_form = $(this);
+  //     var method = $reg_form.attr('method');
+  //     var action = $reg_form.attr('action');
+  //     var data = $reg_form.attr('submit');
+  //     $('.tab-content').hide()
+  // console.log(method)
+  // console.log(action)
+  // console.log(data)
 
-    $.ajax({
-        urls: action,
-        method: method,
-        data: data
-        }).done(function(action) {
+  //   $.ajax({
+  //       urls: action,
+  //       method: method,
+  //       data: data
+  //       }).done(function(action) {
 
-      });
-  });
+  //     });
+  // });
 });
-
-// $(document).ready(function () {
-//   event.preventDefault();
-//   $('.tabs a').on('click', function() {
-
-//   var tabID = $(this).attr('href');
-//     $('.tab-content').hide()
-//     $(tabID).show();
-//     $('.tabs li').removeClass('active');
-//     // debugger;
-//     $(this).parent().addClass('active');
-//     // $(this).attr('href').show ();
-
-
-//   });
-// });
 
 var slideIndex = 1;
 showSlides(slideIndex);
